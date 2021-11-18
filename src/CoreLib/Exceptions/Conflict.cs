7e93f32012112255ee752ce8c2ee331ff215bf68
Furkan <furkan139aydin@gmail.com>
2021-11-19 00:13:08 +0300

    Added Conflict to Exceptions

---
+++ src/CoreLib/Exceptions/Conflict.cs
@@ -0,0 +1,12 @@
+﻿namespace CoreLib.Exceptions
+{
+    public class Conflict : ErrorDetails
+    {
+        public Conflict(int code, string message = null)
+        {
+            Code = code;
+            StatusCode = 409;
+            ErrorMessage = message ?? $"Conflict.";
+        }
+    }
+}
